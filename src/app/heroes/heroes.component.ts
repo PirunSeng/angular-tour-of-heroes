bcc905e308c7c948f073c0fde0e516d7451883ea
PirunSeng <sengpirun.rain@gmail.com>
2019-03-07 15:12:40 +0700

    Section 6. HTTP & CRUD

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,8 +14,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   // selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
-  }
+  constructor(
+    private heroService: HeroService
+
+  ){}
 
   ngOnInit() {
     this.getHeroes();
@@ -31,5 +33,21 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  // add hero
+  add(name: string): void {
+    name = name.trim();
+    if(!name){ return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  // delete hero
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
